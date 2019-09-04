d9ab06a94b058b87b83c4022d7c909a23c87ae69
Matheus Azevedo <matheus.klementt@gmail.com>
2019-09-03 22:28:58 -0300

    starting pedraPapelTesoura game logic

---
+++ pedra-papel-tesoura/pedraPapelTesoura.hs
@@ -0,0 +1,18 @@
+--Jokenpo--
+
+import System.Random
+
+
+optionList = ["pedra", "papel", "tesoura"]
+
+getOption :: Int -> String
+getOption x = optionList !! (x-1)
+
+
+
+selectRandomElement :: [a] -> IO a
+selectRandomElement [] = error "Cannot select an element from an empty list."
+selectRandomElement list = randomIntWithinRange >>=
+  \r -> return $ list !! r
+  where
+  randomIntWithinRange = getStdRandom $ randomR (0, length list - 1)

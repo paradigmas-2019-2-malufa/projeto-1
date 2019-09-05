afbf60a40aa5e1493ab4307099e4bf0a5b7ff531
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-05 11:33:15 -0300

    Fixes and upgrade runGame
    Co-authored-by: FabianaRibas <fabianalv.ribas@gmail.com>

--- jogo-da-velha/jogoDaVelha/jogoDaVelha.hs
+++ jogo-da-velha/jogoDaVelha/jogoDaVelha.hs
@@ -1,13 +1,8 @@
-{-
-Jogo da velha versão 1.0
--}
-
 import Control.Exception
 import System.Process
 import System.IO.Error
 import System.IO
 
---definitions
 type Players = [Player]
 type Turn = Int
 type Name = String
@@ -47,7 +42,7 @@ my_main = do
 
 menu :: Players -> IO Players
 menu option = do
-                system "clear"
+                system "clear || cls"
                 putStrLn "::::::::::::::::::::::::::::::::::::::: Hash Game :::::::::::::::::::::::::::::::::::::::"
                 putStrLn "\n1 - REGISTER A PLAYER"
                 putStrLn "2 - PLAY"
@@ -63,29 +58,23 @@ runOption option '1' = registerPlayer option
 runOption option '2' = prepareGame option
 runOption option '0' = do
     putStrLn("\nThanks for playing!\n")
-    {-go back to manu gameboy-}
     return option
-
-{-
-runOption option '2' = do
-
-runOption option '3' = do
--}
+    --return to selection game
 
 runOption option _ = do
     putStrLn("\nInvalid option! Try again!")
-    putStr ("\n Press <Enter> to continue....")
+    putStr ("\n Press enter to continue....")
     getChar
-    menu option {-go back to hash manu-}
+    menu option
 
 
 registerPlayer :: Players -> IO Players
 registerPlayer option = do
-    putStrLn("\nEnter a user name: \n")
+    putStr("\nEnter a user name: ")
     name <- getLine
     if(validPlayer option name) then do
-        putStrLn "This player already exists, try another..."
-        putStr ("\n Press <Enter> to continue....")
+        putStrLn ("This player "++ (show name) ++" already exists, try another...")
+        putStr ("\n Press enter to continue....")
         getChar
         menu option
     else do
@@ -93,7 +82,7 @@ registerPlayer option = do
         hPutStrLn file (show ((Player name 0): option))
         hClose file
         putStrLn("Player " ++ name ++ " successfully registered!!")
-        putStr("\n Press <Enter> to continue...")
+        putStr("\n Press enter to continue...")
         getChar
         menu ((Player name 0):option)
 
@@ -102,19 +91,21 @@ validPlayer [] _ = False
 validPlayer ((Player n p):xs) name --s:xs
                 |(n == name) = True
                 |otherwise = validPlayer xs name
+
 prepareGame :: Players -> IO Players
 prepareGame option = do 
-        putStrLn "Digit the name of your rival"
+        putStr "What is your name: "
         player1 <- getLine
         if not (validPlayer option player1) then do 
-            putStrLn "\nPlayer not exists!"
+            putStrLn ("\n"++ (show player1) ++ " not exists!")
             putStr "\nPress enter to continue"
             getChar
             menu option
         else do
+            putStr "And the name of your rival: "
             player2 <- getLine
-            if not(validPlayer option player2) then do
-                putStrLn "\nPlayer not exists!"
+            if not (validPlayer option player2) then do
+                putStrLn( "\n"++ (show player2) ++ " not exists!")
                 putStr "\nPress enter to continue"
                 getChar
                 menu option
@@ -123,9 +114,29 @@ prepareGame option = do
 
 newGame :: Players -> Name -> Name -> IO Players
 newGame option player1 player2 = do
-    putStrLn ("\nBegin the game: " ++  player1 ++ " vs "++ player2)
+    putStrLn ("\nBegin the game: " ++  (show player1) ++ " vs "++ player2)
     putStrLn ("\n" ++ player1 ++ " will be \'X\' and "++ player2 ++ "will be \'O\'")
     runGame option ['1'..'9'] player1 player2 0
 
 runGame ::Players -> GameTable -> Name ->Name -> Turn -> IO Players
-runGame option  gtable player1 player2 turn = menu option
+runGame option  gtable player1 player2 turn = do
+    putStrLn ("\n" ++ "                              " ++
+        (show (gtable !! 0)) ++ " | " ++ (show (gtable !! 1)) ++ " | " ++ (show (gtable !! 2)) ++
+        "\n                              ---------------\n" ++ "                              " ++
+        (show (gtable !! 3)) ++ " | " ++ (show (gtable !! 4)) ++ " | " ++ (show (gtable !! 5)) ++
+        "\n                              ---------------\n" ++ "                              " ++
+        (show (gtable !! 6)) ++ " | " ++ (show (gtable !! 7)) ++ " | " ++ (show (gtable !! 8)) ++
+        "\n")
+    getChar
+    menu option
+    -- if (player1 gtable) then do 
+    --     putStrLn ("Congratulations " ++ player1 ++ "! You win!!")
+    --     arq <- openFile "dados.txt" WriteMode
+    --     hPutStrLn arq (show (refreshScore dados player1))
+    --     hClose arq
+    --     arq <- openFile "dados.txt" ReadMode
+    --     dados_atualizados <- hGetLine arq
+    --     hClose arq
+    --     putStr "\nPress enter to return to the menu..."
+    --     getChar
+    --     menu (read dados_atualizados)
